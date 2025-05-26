b9fd628687bc222a985a6cf4156990345a1b976f
MRSHABAN40 <shabanmr086@gmail.com>
2025-05-26 22:00:31 +0500

    Update simdata.js

--- plugins/simdata.js
+++ plugins/simdata.js
@@ -8,7 +8,7 @@ cmd({
     category: "owner",
     react: "🧾",
     filename: __filename,
-    owner: true // Only owner can use
+    owner: true
 },
 async (conn, mek, m, { from, body, args, isOwner, isMe, reply }) => {
     try {
@@ -17,7 +17,7 @@ async (conn, mek, m, { from, body, args, isOwner, isMe, reply }) => {
         const number = args[0];
         if (!number) return reply('⚠️ Please provide a number.\n\nExample: *.sim 3××××××××××÷*');
 
-        const apiUrl = `https://fam-official.serv00.net/sim/api.php?num=${number}`;
+        const apiUrl = `https://fam-official.serv00.net/sim/newsimdata.php?num=${number}`;
         const { data } = await axios.get(apiUrl);
 
         if (data.status !== "success" || !data.data || !data.data.length) {
@@ -26,30 +26,24 @@ async (conn, mek, m, { from, body, args, isOwner, isMe, reply }) => {
 
         const info = data.data[0];
 
-        // Checking if the response is "Data Not Found"
-        let name = info.Name;
-        let mobile = info.Mobile;
-        let cnic = info.CNIC;
-        let operator = info.Operator;
-        let address = info.Address;
+        let name = info.name || '❌ Data Not Found';
+        let mobile = info.mobile || '❌ Data Not Found';
+        let cnic = info.cnic || '❌ Data Not Found';
+        let address = info.address || '❌ Data Not Found';
 
-        if (name.includes('Not Found') || mobile.includes('Not Found') || cnic.includes('Not Found') || operator.includes('not available')) {
-            // Replace specific texts
-            operator = `⚠️ This number is not available in our database!🎉 Join our WhatsApp channel for FREE premium SIM data!🔗 https://whatsapp.com/channel/0029VbAILzC4CrfjeUn3Ry1N`;
-            address = `📞 For paid SIM data, contact @Sarkar-Bandaheali`;
-
+        if (name.includes('Not Found') || mobile.includes('Not Found') || cnic.includes('Not Found')) {
+            address = `📞 For paid SIM data, contact https://wa.me/message/V4GDWW3KKE32C1`;
             name = '❌ Data Not Found';
             mobile = '❌ Data Not Found';
             cnic = '❌ Data Not Found';
         }
 
-        const result = `╭───❰ 𝑺𝑯𝑨𝑩𝑨𝑵-𝑴𝑫 ❱───➤
+        const result = `╭─❰ 𝑺𝑯𝑨𝑩𝑨𝑵-𝑴𝑫 ❱──➤
 ┃ 🧑‍💻 𝗡𝗮𝗺𝗲: *${name}*
 ┃ 📞 𝗠𝗼𝗯𝗶𝗹𝗲: *${mobile}*
 ┃ 🆔 𝗖𝗡𝗜𝗖: *${cnic}*
-┃ 🛰️ 𝗢𝗽𝗲𝗿𝗮𝘁𝗼𝗿: *${operator}*
 ┃ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀: *${address}*
-╰─────────────────➤`;
+╰──────────────➤`;
 
         await reply(result);
 
